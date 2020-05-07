896ddc9aadc7491fb35f3bb00a0a4ff761282d91
Nitesh Soni <63317231+codeNitesh@users.noreply.github.com>
2020-05-08 02:24:47 +0530

    10- Next Alphabet in a sorted array

--- Next-Alphabet.java
+++ Next-Alphabet.java
@@ -1,6 +1,7 @@
 package ***;
 
 public class NextAlphabet {
+
 	public static void main(String[] args) {
 		char arr[] = {'b', 'f', 'k', 'm', 'n', 'r', 'u', 'x', 'z'};
 		char toget = 'y';
@@ -8,13 +9,13 @@ public class NextAlphabet {
 		
 		int result = nextAlpha(arr, start, end, toget);
 		System.out.println(arr[result]);
+
 	}
 
 	private static int nextAlpha(char[] arr, int start, int end, char toget) {
 		while(start <= end) {
 			int mid = start+(end-start)/2;
-			if(arr[mid] == toget) return mid;
-			else if(arr[mid] > toget) {
+			if(arr[mid] > toget) {
 				end = mid-1;
 				if(arr[mid-1] < toget) return mid;
 			}else {
@@ -24,4 +25,5 @@ public class NextAlphabet {
 		}
 		return 0;
 	}
+
 }
